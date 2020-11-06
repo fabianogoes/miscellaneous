a60a4bae8325797f1d30316b5928f8e732ce63f9
Fabiano Góes <fabianogoes@gmail.com>
2020-11-05 22:33:17 -0300

    add vagrant exemplo

---
+++ java/vagrant-ubuntu-tomcat7/README.md
@@ -0,0 +1,51 @@
+# Java/JEE development environment
+
+This vagrant image will kick start your development in a self contained ubuntu precise32 box with all the neccessities installed. The developers just need to configure their eclipse to the shared tomcat filesystem and it's "hakuna matata" all over!
+Will add more instructions with a sample project in the next few days.
+
+## Pre-requisites
+Ensure you have the following tools installed:
+* virtualbox - https://www.virtualbox.org/
+* vagrant - http://www.vagrantup.com/
+* URL to current apache-tomcat-7.x.x.tar.gz (Ex http://apache.mirrors.pair.com/tomcat/tomcat-7/v7.0.50/bin/apache-tomcat-7.0.52.tar.gz)
+	* Update the `$tomcat_url` definition in `manifests/default.pp` if the url is out of date.
+* librarian-puppet - https://github.com/rodjek/librarian-puppet
+	* puppet installation is optional, the modules have been added as gitsubmodules and pushed to the repo. It's necessary only if you think the modules are outdated
+
+## Vagrant Setup
+###Do the following:
+* $ ```vagrant box add precise32 http://files.vagrantup.com/precise32.box```
+	* This will download the VM for you
+* $ ```git clone https://github.com/seshendra/vagrant-ubuntu-tomcat7.git```
+	* clone this repoistory (it's your working vagrant location)
+* **If librarian-puppet is installed**, grab the puppet modules:
+	* $ ```cd vagrant-ubuntu-tomcat7/manifests```
+	* $ ```librarian-puppet install```
+	* $ ```cd ..```
+
+* Otherwise, **if librarian-puppet is not installed**, clone the puppet modules
+	* $ ```cd vagrant-ubuntu-tomcat7```
+	* $ ```git submodule init```
+  	* $ ```git submodule update```
+
+* $ ```vagrant up```
+	* brings up the VM with tomcat and java installed.
+	* This can take anywhere between 20-30 minutes, so issue the command and go have some coffee or attend a meeting or watch a video while vagrant does it's job.
+	* If you are in a VPN, ensure *ubuntu.com and *apache.com are open for downloads in your organization.
+* $ ```vagrant ssh```
+	* Login to your instance.
+
+## Deployment Details
+* Tomcat is set at auto-start to false
+  * use ```sudo supervisorctl start tomcat``` to start tomcat
+* Vagrant is setup to map port 8080 of the VM to port 4880 on your machine
+	*  http://localhost:4880/
+* JMX support is enabled on the server: Vagrant is setup to map port 1099 of the VM to port 1099 on your machine, allowing for monitoring and remote deployment.
+* Tomcat is started in debug mode: Vagrant is setup to map port 8000 of the VM to port 4800 on your machine, allowing for debugging from your IDE.
+
+## Package as a box for customizing in your projects
+* After box is configured and provisioned, you can package and use this as your base box to speed up your subsequent reloads
+* ```vagrant package```
+* ```mv package.box precise32-maven-tomcat7.box```
+* ```vagrant box add precise32-maven-tomcat7 precise32-maven-tomcat7.box```
+* Use ```precise32-maven-tomcat7``` as the name of the box in your VagrantFile ```config.vm.box = "precise32-maven-tomcat7"```
---
+++ java/vagrant-ubuntu-tomcat7/manifests/default.pp
@@ -0,0 +1,168 @@
+class java-development-env {
+  include apt
+  include maven
+
+  apt::ppa { "ppa:webupd8team/java": }
+
+  # Set current Tomcat download url.
+  $tomcat_url = "http://artfiles.org/apache.org/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz"
+
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  exec { 'apt-get update 2':
+    command => '/usr/bin/apt-get update',
+    require => [ Apt::Ppa["ppa:webupd8team/java"], Package["git-core"] ],
+  }
+
+  # install necessary ubuntu packages to setup the environment
+  package { ["vim",
+             "curl",
+             "git-core",
+             "expect",
+             "bash"]:
+    ensure => present,
+    require => Exec["apt-get update"],
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  package { ["oracle-java7-installer"]:
+    ensure => present,
+    require => Exec["apt-get update 2"],
+  }
+
+  exec {
+    "accept_license":
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+    cwd => "/home/vagrant",
+    user => "vagrant",
+    path    => "/usr/bin/:/bin/",
+    require => Package["curl"],
+    before => Package["oracle-java7-installer"],
+    logoutput => true,
+  }
+
+  $server = {
+    id => "remote-tomcat-server",
+    username => "admin",
+    password => "tomcat",
+  }
+
+  maven::settings { 'mvn-settings' :
+    local_repo => '/vagrant/maven/.m2/repository',
+    servers    => [$server],
+  }
+
+  Exec {
+    path  => "${::path}",
+  }
+
+  group { "puppet":
+    ensure  => present,
+  }
+
+  package { "acpid":
+    ensure  => installed,
+  }
+
+  package { "supervisor":
+    ensure  => installed,
+  }
+
+  package { "wget":
+    ensure  => installed,
+  }
+
+  user { "vagrant":
+    ensure    => present,
+    comment   => "Tomcat User",
+    home      => "/home/vagrant",
+    shell     => "/bin/bash",
+  }
+
+  exec { "check_tomcat_url":
+    cwd       => "/tmp",
+    command   => "wget -S --spider ${tomcat_url}",
+    timeout   => 900,
+    require   => Package["wget"],
+    notify    => Exec["get_tomcat"],
+    logoutput => "on_failure"
+  }
+
+  exec { "get_tomcat":
+    cwd       => "/tmp",
+    command   => "wget ${tomcat_url} -O tomcat.tar.gz > /opt/.tomcat_get_tomcat",
+    creates   => "/opt/.tomcat_get_tomcat",
+    timeout   => 900,
+    require   => Package["wget"],
+    notify    => Exec["extract_tomcat"],
+    logoutput => "on_failure"
+  }
+
+  exec { "extract_tomcat":
+    cwd         => "/vagrant",
+    command     => "tar zxf /tmp/tomcat.tar.gz ; mv apache* tomcat",
+    creates     => "/vagrant/tomcat",
+    require     => Exec["get_tomcat"],
+    refreshonly => true,
+  }
+
+  file { "/vagrant/tomcat/conf/tomcat-users.xml":
+    ensure    => present,
+    content   => "<?xml version='1.0' encoding='utf-8'?>
+  <tomcat-users>
+    <role rolename=\"manager-gui\" />
+    <role rolename=\"manager-script\" />
+    <role rolename=\"manager-jmx\" />
+    <role rolename=\"manager-status\" />
+    <user username=\"admin\" password=\"tomcat\" roles=\"manager-gui, manager-script, manager-jmx, manager-status\"/>
+  </tomcat-users>",
+    require   => Exec["extract_tomcat"],
+  }
+
+  file { "/vagrant/tomcat":
+    ensure    => directory,
+    owner     => "vagrant",
+    mode      => 0755,
+    recurse   => true,
+    require   => Exec["extract_tomcat"],
+  }
+
+  file { "/vagrant/tomcat/bin/setenv.sh":
+    ensure    => present,
+    owner     => "vagrant",
+    mode      => 0755,
+    content   => '#!/bin/sh
+export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=192.168.33.10"
+export CATALINA_OPTS="$CATALINA_OPTS -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n"
+echo "Using CATALINA_OPTS:"
+for arg in $CATALINA_OPTS
+do
+    echo ">> " $arg
+done
+echo ""',
+    require   => Exec["extract_tomcat"],
+  }
+
+  file { "/etc/supervisor/conf.d/tomcat.conf":
+    ensure    => present,
+    content   => "[program:tomcat]
+command=/vagrant/tomcat/bin/catalina.sh run
+directory=/vagrant/tomcat/bin
+autostart=no
+user=vagrant
+stopsignal=QUIT",
+    require   => [ Package["supervisor"], File["/vagrant/tomcat/conf/tomcat-users.xml"] ],
+    notify    => Exec["update_supervisor"],
+  }
+
+  exec { "update_supervisor":
+    command     => "supervisorctl update",
+    refreshonly => true,
+  }
+
+}
+
+include java-development-env
